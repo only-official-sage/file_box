f77974bbf3864c01cb989570c9107c0cc9a8a83e
only_official_sage <ezeonyedikachi41@gmail.com>
2025-02-04 16:04:57 +0100

    updated the index.js file to render taste

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import livereload from "livereload";
 import connectLivereload from "connect-livereload";
-import serverless from "serverless-http";
+// import serverless from "serverless-http";
 import express from "express";
 import { dirname } from 'path';
 import session from "express-session";
@@ -22,11 +22,12 @@ app.use(session({
   secret: process.env.SESSION_SECRETE,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: true }
 }))
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -37,43 +38,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use('/files', express.static(path.join(__dirname, '..', 'folders')));
 
-// Live Reload
-const liveReloadServer = livereload.createServer({
-  exts: ['ejs', 'css', 'js']
-});
-
- 
-liveReloadServer.watch([`${__dirname}/public`, `${__dirname}/views`]);
-
-app.use(connectLivereload());
-
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/"); 
-  }, 1000);
-});
   
-// API Routes 
-app.use("/api", user);
 
-// Pages Routes
-app.use("/", pages);
-
-// Auth Routes
-app.use('/auth', auth) 
- 
-if (process.env.NODE_ENV == 'DEV') {
-  mongoose.connect('mongodb://localhost:27017/file_box',).then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server @ http://localhost:${PORT}`);
-    }); 
-  })
-} else {
-  mongoose.connect('mongodb+srv://bradhunt408:aqsfc55sVIemikeO@cluster0.vdoxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/file_box').then(() => {
-    console.log('connected on DB');
-  })
+if (process.env.NODE_ENV === 'development') {
+  // Live Reload
+  const liveReloadServer = livereload.createServer({ exts: ['ejs', 'css', 'js'] });
+  liveReloadServer.watch([`${__dirname}/public`, `${__dirname}/views`]);
+  app.use(connectLivereload());
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => liveReloadServer.refresh("/"), 1000);
+  });
 }
 
 
+// API Routes 
+app.use("/api", user);
+// Pages Routes
+app.use("/", pages);
+// Auth Routes
+app.use('/auth', auth) 
+ 
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/file_box', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+  });
+
 // Export for serverless
-export const handler = serverless(app);
+// export const handler = serverless(app);
